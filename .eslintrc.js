0873320fec00386a228ebb54c5aa9a1d7bbf0138
TMK04 <floer.learner.01@gmail.com>
2023-04-25 13:38:56 +0800

    ci(format script): change to & run eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,44 +4,50 @@
  * @type { LinterConfig & { settings: { "import/resolver": { typescript: TsResolverOptions } } } }
  */
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-		'plugin:import/recommended',
-		'plugin:import/typescript',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: true
-	},
-	plugins: ['@typescript-eslint'],
-	rules: {
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-floating-promises': 'off',
-		'@typescript-eslint/no-non-null-assertion': 'off',
-		'@typescript-eslint/no-unsafe-argument': 'off',
-		'@typescript-eslint/no-unsafe-assignment': 'off',
-		'@typescript-eslint/no-unsafe-call': 'off',
-		'import/order': [
-			'warn',
-			{
-				groups: ['type', ['external', 'builtin'], ['internal', 'parent'], ['index', 'sibling'], 'object'],
-				'newlines-between': 'always',
-				alphabetize: {
-					order: 'asc',
-					orderImportKind: 'asc'
-				}
-			}
-		],
-		'sort-imports': ['warn', { ignoreDeclarationSort: true }]
-	},
-	settings: {
-		'import/resolver': {
-			typescript: true
-		}
-	},
-	ignorePatterns: ['dist/', '**/*.js']
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "import/order": [
+      "warn",
+      {
+        "groups": [
+          "type",
+          ["external", "builtin"],
+          ["internal", "parent"],
+          ["index", "sibling"],
+          "object"
+        ],
+        "newlines-between": "always",
+        "alphabetize": {
+          order: "asc",
+          orderImportKind: "asc"
+        }
+      }
+    ],
+    "sort-imports": ["warn", { ignoreDeclarationSort: true }]
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true
+    }
+  },
+  ignorePatterns: ["dist/", "**/*.js"]
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-	$schema: 'http://json.schemastore.org/prettierrc',
-	printWidth: 100,
-	quoteProps: 'consistent',
-	trailingComma: 'none',
-	embeddedLanguageFormatting: 'auto'
+  $schema: "http://json.schemastore.org/prettierrc",
+  printWidth: 100,
+  quoteProps: "consistent",
+  trailingComma: "none",
+  embeddedLanguageFormatting: "auto"
 };
